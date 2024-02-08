4fd0874f7eaf1d06c81639979f84fb7550f226c7
Iliyajhushen <iliyajhushen84@gmail.com>
2024-02-08 14:30:48 +0530

    added content

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -5,6 +5,10 @@ const Index = () => {
     <div>
       <h1>About page </h1>
       <h1>Add Functionality page </h1>
+      <h1>Add Functionality page2 </h1>
+      <h1>Add Functionality page3 </h1>
+
+
 
     </div>
   )
